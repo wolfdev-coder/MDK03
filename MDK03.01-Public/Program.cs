8f026bcaf44cd564ea12ec90136f99d87a9b5ad0
WolfDev <94852388+wolfdev-coder@users.noreply.github.com>
2024-09-12 11:07:36 +0400

    Add files via upload
    Это первая версия программы

---
+++ MDK03.01-Public/Program.cs
@@ -0,0 +1,18 @@
+namespace MDK03._01_Public
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize(); //hello
+
+            Application.Run(new Form1());
+        }
+    }
+}
